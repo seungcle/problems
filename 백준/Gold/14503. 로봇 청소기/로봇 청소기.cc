6e860e4830eb815adaf7c985fd3f69be9aaa4428
이승찬 <145752978+seungcle@users.noreply.github.com>
2026-04-08 12:17:39 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 50
+int N, M;
+int visited_count;
+int map[MAX][MAX]; // 지도
+int visited[MAX][MAX] = {
+    0,
+}; 
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int r, c, d;
+
+void Input()
+{
+    cin >> N >> M;
+    cin >> r >> c >> d;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+
+    visited[r][c] = 1;
+    visited_count++;
+}
+
+void dfs()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        int next_d = (d + 3 - i) % 4; 
+        int next_r = r + dx[next_d];
+        int next_c = c + dy[next_d];
+
+        if (next_r < 0 || next_r >= N || next_c < 0 || next_c >= M || map[next_r][next_c] == 1)
+        {
+            continue;
+        }
+
+        if (map[next_r][next_c] == 0 && visited[next_r][next_c] == 0)
+        {
+            visited[next_r][next_c] = 1;
+            r = next_r;
+            c = next_c;
+            d = next_d;
+            visited_count++;
+            dfs();
+        }
+    }
+
+    int back_idx = d > 1 ? d - 2 : d + 2;
+    int back_r = r + dx[back_idx];
+    int back_c = c + dy[back_idx];
+
+    if (back_r >= 0 && back_r <= N || back_c >= 0 || back_c <= M)
+    {
+        if (map[back_r][back_c] == 0)
+        {
+            r = back_r;
+            c = back_c;
+            dfs();
+        }
+        else
+        {
+            cout << visited_count << endl;
+            exit(0);
+        }
+    }
+}
+
+int main()
+{
+    Input();
+    dfs();
+
+    return 0;
+}
