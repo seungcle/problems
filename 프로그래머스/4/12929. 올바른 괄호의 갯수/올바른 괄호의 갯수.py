ffda2eefca9e75918867ba3abdea95a9cdcf70bb
이승찬 <145752978+seungcle@users.noreply.github.com>
2026-05-27 16:38:07 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,8 +1,3 @@
 def solution(n):
-    dp = [0] * 15
-    dp[0] = 1
-    dp[1] = 1
-    for j in range(2,n+1):
-        for i in range(0,j):
-            dp[j] = dp[j] + dp[i] * dp[j-1-i]
-    return dp[n]
+    answer = 0
+    return answer
